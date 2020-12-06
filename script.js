f4f98ead3c043ce96d153fdd5e4826bdde204395
Jozdien <joseverres42@gmail.com>
2020-12-06 23:56:39 +0530

    Landing Page Mobile View; Navbar

--- script.js
+++ script.js
@@ -16,3 +16,39 @@ function splash(param) {
 	  m.style.display = "block";
 	}, time);
 }
+
+$(function () {
+	'use strict'
+	$("[data-trigger]").on("click", function(){
+    var trigger_id =  $(this).attr('data-trigger');
+    $(trigger_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+    var name = document.getElementById("landing-name-container");
+    name.style.visibility = "hidden";
+   });
+
+    // close if press ESC button 
+  $(document).on('keydown', function(event) {
+    if(event.keyCode === 27) {
+      $(".navbar-collapse").removeClass("show");
+      $("body").removeClass("overlay-active");
+      var name = document.getElementById("landing-name-container");
+    	name.style.visibility = "visible";
+    }
+  });
+
+    // close button 
+  $(".btn-close").click(function(e){
+    $(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+    var name = document.getElementById("landing-name-container");
+    name.style.visibility = "visible";
+  });
+
+  $(".nav-link").click(function(e){
+  	$(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+    var name = document.getElementById("landing-name-container");
+    name.style.visibility = "visible";
+  });
+})
